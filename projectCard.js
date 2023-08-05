251e7ef770e576a0c67303a68ae8f77b022c004a
elielg <eliel.granados@outlook.com>
2023-08-05 11:09:43 -0700

    final version

--- projectCard.js
+++ projectCard.js
@@ -2,7 +2,6 @@ class ProjectCard extends HTMLElement {
     constructor() {
         super();
 
-        // Sample JSON data
         const jsonData = {
             title: 'Local Storage Load',
             imageSource: 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTbX4kyAKSWkOT_amdu2gC9C2RKjaE6ZQrnLOpK1XSNy41NC0cSZw6eVVBW4lPAAJe_YcF2UAvDhp8pQxc',
@@ -10,10 +9,8 @@ class ProjectCard extends HTMLElement {
             linkUrl: 'https://en.wikipedia.org/wiki/University_of_California,_San_Diego'
         };
 
-        // Convert JSON data to a string
         const jsonString = JSON.stringify(jsonData);
 
-        // Save the stringified JSON data to localStorage
         localStorage.setItem('localCard', jsonString);
 
     }
@@ -30,26 +27,48 @@ class ProjectCard extends HTMLElement {
         /* News Card */
         .card {
             display: flex;
-            background-color: #90AFC5;
+            flex-direction: column;
+            gap: 10%;
+            background-color: #557A95;
             border: 5px solid #2A3132; /* Add a border */
-            width: 50%;
+            width: 20%;
+            border-radius: 5%; 
+            justify-content: center; /* Center items horizontally along the main axis */
+        align-items: center; /
+        
+
 
         }
       
         .card-inner{
+            
             text-align: center;
-            width:80%;
+            justify-content: center; /* Center horizontally */
+            align-items: center; /* Center vertically */
+            width:70%;
+            background-color: #90AFC5;
+            margin-bottom: 4%;
+            padding-bottom: 4%;
+            border: 5px solid #2A3132; /* Add a border */
+            border-radius: 5%; 
+            box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.5); 
+
             
         }
     
         .img{
-            max-width: 50%;
-            border-radius: 50%; /* Make the image circular */
-            overflow: hidden; /* Hide any overflow content */
+            max-width: 90%;
+            border-radius: 5%; 
+            border: 5px solid #2A3132; /* Add a border */
+            overflow: hidden; 
             margin: 20px;
         }
         
-    
+        .button{
+            background-color: #F56C57;
+            border: 2px solid #2A3132; /* Add a border */
+
+        }
        
 
         </style>
@@ -63,24 +82,23 @@ class ProjectCard extends HTMLElement {
                 <p>${this.getAttribute('Description')}</p>
                 <a href="${this.getAttribute('linkUrl')}" class="link-button" target="_blank">Read More</a>
                 <br>
-                <button class="local-button">Load Local</button>
-                <button class="storage-button">Load Storage</button>
+                <button id="local-button" class="button">Load Local</button>
+                <button id="storage-button" class="button">Load Storage</button>
           </section>
           </section>
       </section>
         `;
 
-        const localButton = this.querySelector('.local-button');
-        const storageButton = this.querySelector('.storage-button');
+        const localButton = this.querySelector('#local-button');
+        const storageButton = this.querySelector('#storage-button');
 
         localButton.addEventListener('click', () => {
-            // Update the Description attribute with a new value
+
             this.setAttribute('description', 'New local description');
-            // Call render again to update the HTML content
+
             const storedData = localStorage.getItem('localCard');
             const jsonData = JSON.parse(storedData);
 
-            // Check if the data exists in localStorage
             for (const key in jsonData) {
                 if (jsonData.hasOwnProperty(key)) {
                     console.log(`Attribute: ${key}, Value: ${jsonData[key]}`);
@@ -92,13 +110,10 @@ class ProjectCard extends HTMLElement {
 
         storageButton.addEventListener('click', () => {
 
-            // Replace 'API_KEY' with your actual JSONbin.io API key
             const apiKey = '$2b$10$Krbk7mlsC7PF1zs0pLUtS.YKK3zRY/5Dp7M7VmwfOHkIEvHj29Czq';
 
-            // Construct the URL for fetching data
             const url = `https://api.jsonbin.io/v3/b/64ce728db89b1e2299cbdce5`;
 
-            // Set headers for authentication
             const headers = {
                 'X-Access-Key': apiKey,
                 'Content-Type': 'application/json',
