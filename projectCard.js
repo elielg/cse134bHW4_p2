b9fc6a2d55aa95dc1576ef52a8b9b7f49f86dd94
elielg <eliel.granados@outlook.com>
2023-08-05 10:16:04 -0700

    final version

--- projectCard.js
+++ projectCard.js
@@ -1,83 +1,138 @@
-const template = document.createElement('template');
-template.innerHTML = `
-  <style>
-  .user-card {
-		font-family: 'Arial', sans-serif;
-		background: #f4f4f4;
-		width: 500px;
-		display: grid;
-		grid-template-columns: 1fr 2fr;
-		grid-gap: 10px;
-		margin-bottom: 15px;
-		border-bottom: darkorchid 5px solid;
-	}
+class ProjectCard extends HTMLElement {
+    constructor() {
+        super();
 
-	.user-card img {
-		width: 100%;
-	}
+        // Sample JSON data
+        const jsonData = {
+            title: 'Local Storage Load',
+            imageSource: 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTbX4kyAKSWkOT_amdu2gC9C2RKjaE6ZQrnLOpK1XSNy41NC0cSZw6eVVBW4lPAAJe_YcF2UAvDhp8pQxc',
+            Description: 'This description was loaded from local storage',
+            linkUrl: 'https://youtu.be/dQw4w9WgXcQ'
+        };
 
-	.user-card button {
-		cursor: pointer;
-		background: darkorchid;
-		color: #fff;
-		border: 0;
-		border-radius: 5px;
-		padding: 5px 10px;
-	}
-  </style>
-  <div class="user-card">
-    <img />
-    <div>
-      <h3></h3>
-      <div class="info">
-        <p><slot name="email" /></p>
-        <p><slot name="phone" /></p>
-      </div>
-      <button id="toggle-info">Hide Info</button>
-    </div>
-  </div>
-`;
+        // Convert JSON data to a string
+        const jsonString = JSON.stringify(jsonData);
 
-class UserCard extends HTMLElement {
-  constructor() {
-    super();
+        // Save the stringified JSON data to localStorage
+        localStorage.setItem('localCard', jsonString);
 
-    this.showInfo = true;
-
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-    const name = this.getAttribute('name');
-    const avatar = this.getAttribute('avatar');
-    console.log('Name:', name);
-    console.log('Avatar:', avatar);
-
-    this.shadowRoot.querySelector('h3').innerText = name;
-    this.shadowRoot.querySelector('img').src = avatar;
-  }
-
-  toggleInfo() {
-    this.showInfo = !this.showInfo;
-
-    const info = this.shadowRoot.querySelector('.info');
-    const toggleBtn = this.shadowRoot.querySelector('#toggle-info');
-
-    if(this.showInfo) {
-      info.style.display = 'block';
-      toggleBtn.innerText = 'Hide Info';
-    } else {
-      info.style.display = 'none';
-      toggleBtn.innerText = 'Show Info';
     }
-  }
 
-  connectedCallback() {
-    this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
-  }
+    connectedCallback() {
+        this.render();
+    }
 
-  disconnectedCallback() {
-    this.shadowRoot.querySelector('#toggle-info').removeEventListener();
-  }
+
+
+    render() {
+        this.innerHTML = `
+        <style>
+        /* News Card */
+        .card {
+            display: flex;
+            background-color: #90AFC5;
+            border: 5px solid #2A3132; /* Add a border */
+            width: 50%;
+
+        }
+      
+        .card-inner{
+            text-align: center;
+            width:80%;
+            
+        }
+    
+        .img{
+            max-width: 50%;
+            border-radius: 50%; /* Make the image circular */
+            overflow: hidden; /* Hide any overflow content */
+            margin: 20px;
+        }
+        
+    
+       
+
+        </style>
+
+
+        <section class="container">
+          <section class="card">
+          <img src="${this.getAttribute('imageSource')}" class="img" />
+          <section class="card-inner">
+                <h2 class="title">${this.getAttribute('title')}</h2>
+                <p>${this.getAttribute('Description')}</p>
+                <a href="${this.getAttribute('linkUrl')}" class="link-button" target="_blank">Read More</a>
+                <br>
+                <button class="local-button">Load Local</button>
+                <button class="storage-button">Load Storage</button>
+          </section>
+          </section>
+      </section>
+        `;
+
+        const localButton = this.querySelector('.local-button');
+        const storageButton = this.querySelector('.storage-button');
+
+        localButton.addEventListener('click', () => {
+            // Update the Description attribute with a new value
+            this.setAttribute('description', 'New local description');
+            // Call render again to update the HTML content
+            const storedData = localStorage.getItem('localCard');
+            const jsonData = JSON.parse(storedData);
+
+            // Check if the data exists in localStorage
+            for (const key in jsonData) {
+                if (jsonData.hasOwnProperty(key)) {
+                    console.log(`Attribute: ${key}, Value: ${jsonData[key]}`);
+                    this.setAttribute(`${key}`, `${jsonData[key]}`);
+                }
+            }
+            this.render();
+        });
+
+        storageButton.addEventListener('click', () => {
+
+            // Replace 'API_KEY' with your actual JSONbin.io API key
+            const apiKey = '$2b$10$Krbk7mlsC7PF1zs0pLUtS.YKK3zRY/5Dp7M7VmwfOHkIEvHj29Czq';
+
+            // Construct the URL for fetching data
+            const url = `https://api.jsonbin.io/v3/b/64ce728db89b1e2299cbdce5`;
+
+            // Set headers for authentication
+            const headers = {
+                'X-Access-Key': apiKey,
+                'Content-Type': 'application/json',
+            };
+
+            // Fetch data from JSONbin.io
+            fetch(url, {
+                method: 'GET',
+                headers: headers
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.record);
+                    // Parse the fetched JSON data
+                    const parsedData = data.record;
+
+                    // Enumerate each key and attribute
+                    for (const key in parsedData) {
+                        if (parsedData.hasOwnProperty(key)) {
+                            console.log(`Key: ${key}, Value: ${parsedData[key]}`);
+                            this.setAttribute(`${key}`, `${parsedData[key]}`);
+
+                        }
+                    }
+                    this.render();
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+            
+        });
+    }
+
+
 }
 
-window.customElements.define('user-card', UserCard);
+window.customElements.define('project-card', ProjectCard);
